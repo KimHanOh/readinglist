695a65868b53344b864525436165c76dc63b491f
KimHanOh <77518811+KimHanOh@users.noreply.github.com>
2021-01-23 23:38:33 +0900

    Update ReadingListController.java

--- src/main/java/readinglist/ReadingListController.java
+++ src/main/java/readinglist/ReadingListController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
-	private static final String reader="craig";
+	private static final String reader="craigssssssss";
 	private ReadingListRepository readingListRepository;
 	
 	@Autowired
